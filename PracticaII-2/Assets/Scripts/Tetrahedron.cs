4633a343fee9e59fac841dd9decca777f043ea60
carlossantayana <carlossantayanavicente@gmail.com>
2024-05-03 20:05:46 +0200

    Falta id

--- PracticaII-2/Assets/Scripts/Tetrahedron.cs
+++ PracticaII-2/Assets/Scripts/Tetrahedron.cs
@@ -19,21 +19,26 @@ namespace Assets.Scripts
             this.node2 = node2;
             this.node3 = node3;
 
-            CalculateVolume();
+            volume = CalculateVolume(this.node0.pos, this.node1.pos, this.node2.pos, this.node3.pos);
         }
 
-        private void CalculateVolume()
+        private float CalculateVolume(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
         {
-            Vector3 A = node1.pos - node0.pos;
-            Vector3 B = node2.pos - node0.pos;
-            Vector3 C = node3.pos - node0.pos;
+            Vector3 A = p1 - p0;
+            Vector3 B = p2 - p0;
+            Vector3 C = p3 - p0;
 
-            volume = Mathf.Abs(Vector3.Dot(A, Vector3.Cross(B, C))) / 6;
+            return Mathf.Abs(Vector3.Dot(A, Vector3.Cross(B, C))) / 6;
         }
 
         public Vector4 CalculateBarycentricCoordinates(Vector3 P)
         {
+            float w0 = CalculateVolume(P, node1.pos, node2.pos, node3.pos) / volume;
+            float w1 = CalculateVolume(node0.pos, P, node2.pos, node3.pos) / volume;
+            float w2 = CalculateVolume(node0.pos, node1.pos, P, node3.pos) / volume;
+            float w3 = CalculateVolume(node0.pos, node1.pos, node2.pos, P) / volume;
 
+            return new Vector4(w0, w1, w2, w3);
         }
     }
 }
